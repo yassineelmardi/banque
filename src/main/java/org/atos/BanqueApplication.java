153373099a6025667cf1eb113b40024bb821c2d6
[a613363] Yassine El Mardi <yassine.elmardi@atos.net>
2018-03-23 14:46:04 +0000

    first commit

---
+++ src/main/java/org/atos/BanqueApplication.java
@@ -0,0 +1,12 @@
+package org.atos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanqueApplication.class, args);
+	}
+}
